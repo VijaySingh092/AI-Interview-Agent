1be39b8774759148b41982afd18b6527982ab532
VijaySingh092 <vijaykhetwal372@gmail.com>
2026-09-11 21:39:06 +0530

    server setup

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const app = express()
+
+const PORT= process.env.PORT || 6000
+
+app.get('/',(req,res)=>{
+    return res.json({message:"Server Started"})
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server running on port ${PORT}`)
+})
