26b74fadcc5905fc020d5f41a74bb778edb7529c
VijaySingh092 <vijaykhetwal372@gmail.com>
2026-09-13 19:12:31 +0530

    auth page

---
+++ frontend/src/pages/Auth.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { FaRobot } from "react-icons/fa";
+import { IoSparkles } from "react-icons/io5";
+import { motion } from "motion/react"
+import { FcGoogle } from "react-icons/fc";
+
+const Auth = () => {
+  return (
+    <div className='w-full min-h-screen bg-[#f3f3f3] flex items-center justify-center px-6 py-20 '>
+      <motion.div 
+      initial={{opacity:0,y:-50}}
+      animate={{ opacity:1,y:0}}
+      transition={{duration:1}}
+      className='w-full max-w-md p-8 rounded-3xl bg-amber-50 shadow-2xl border border-gray-200'>
+        <div className='flex items-center justify-center gap-3 mb-6'>
+            <div className='bg-black text-white p-2 rounded-lg'>
+                <FaRobot size={18}/>
+            </div>
+            <h2 className='font-semibold text-lg'>InterviewAi</h2>
+            </div>
+            <h1 className='text-2xl md:text-3xl font-semibold text-center leading-snug mb-4'>Continue with
+                <span className='bg-green-100 text-green-600 px-3 py-1 rounded-full inline-flex items-center gap-2'>
+                <IoSparkles size={16}/>AI smart interview</span>
+            </h1>
+        <p className='text-gray-500 text-center text-sm md:text-base leading-relaxed mb-8'> Sign in to start Ai-powered mock interview,track your progress, and unlock detailed performaces insights.</p>
+        <motion.button
+        whileHover={{opacity:0.9,scale:1.03}}
+        whileTap={{opacity:1,scale:0.98}}
+         className='w-full flex items-center justify-center gap-3 py-3 bg-black text-white rounded-full shadow-md'>
+        <FcGoogle size={20} />
+        Continue with google
+        </motion.button>
+      </motion.div>
+    </div>
+  )
+}
+
+export default Auth
