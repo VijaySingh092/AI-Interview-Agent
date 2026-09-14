409909fe063205b8999fcf41e042b4079818551d
VijaySingh092 <vijaykhetwal372@gmail.com>
2026-09-14 18:59:13 +0530

    redux setup

--- frontend/src/pages/Auth.jsx
+++ frontend/src/pages/Auth.jsx
@@ -7,8 +7,11 @@ import { linkWithCredential, signInWithPopup } from 'firebase/auth';
 import { auth, provider } from '../utils/firebase';
 import { ServerUrl } from '../App';
 import axios from 'axios'
+import { useDispatch } from 'react-redux';
+import { setUserData } from '../redux/userSlice';
 
 const Auth = () => {
+    const dispatch = useDispatch()
 
     const handleGoogleAuth = async()=>{
         try{
@@ -20,9 +23,11 @@ const Auth = () => {
 
             const result = await axios.post(ServerUrl +"/api/auth/google",{name,email},{withCredentials:true})
             // console.log(result.data)
+            dispatch(setUserData(result.data))
             
         }catch(error){
             console.log(`Google auth error : ${error}`)
+            dispatch(setUserData(null))
         }
     }
   return (
