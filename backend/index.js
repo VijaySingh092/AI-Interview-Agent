015fd53ab4cd50c14a25a0f15bce7d7590c5fd8c
VijaySingh092 <vijaykhetwal372@gmail.com>
2026-09-15 20:23:41 +0530

    interview controller

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ import cookieParser from 'cookie-parser'
 import cors from 'cors'
 import authRouter from './routes/auth.route.js'
 import userRouter from './routes/user.route.js'
+import interviewRouter from './routes/interview.route.js'
 
 const app = express()
 const PORT= process.env.PORT || 6000
@@ -20,6 +21,8 @@ app.use(cookieParser())
 
 app.use("/api/auth",authRouter)
 app.use("/api/user",userRouter)
+
+app.use("/api/interview",interviewRouter)
 
 app.get('/',(req,res)=>{
     return res.json({message:"Server Started"})
