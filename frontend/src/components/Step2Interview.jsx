c4dcb34e625cad7a4e3ccbd28c3ca6b697d6990c
VijaySingh092 <vijaykhetwal372@gmail.com>
2026-09-21 10:41:54 +0530

    subtitle feature

--- frontend/src/components/Step2Interview.jsx
+++ frontend/src/components/Step2Interview.jsx
@@ -116,6 +116,37 @@ window.speechSynthesis.speak(utterance)
   })
   }
 
+  useEffect(()=>{
+    if(!selectedVoice){
+      return
+    }
+    const runIntro=async ()=>{
+      if(isIntroPhase){
+        await speakText(
+          `Hi ${userName}, it's great to meet you today. I hope you're feeling confident and ready.`
+        )
+
+        await speakText(
+          "I'll ask you a few questions. Just answer naturally, and take your time. Let's begin."
+        )
+
+        setIntroPhase(false)
+      }else if(currentQuestion){
+        await new Promise(r=>setTimeout(r,800))
+
+        // If last question (hard level)
+        if(currentIndex=== questions.length-1){
+          await speakText("Alright, this one might be a bit more challenging.")
+        }
+
+        await speakText(currentQuestion.question)
+      }
+    }
+
+    runIntro()
+
+  },[selectedVoice, isIntroPhase, currentIndex])
+
   
   return (
     <div className='min-h-screen bg-linear-to-br from-emerald-50 via-white to-teal-100 flex items-center justify-center p-4 sm:p-6'>
@@ -135,6 +166,13 @@ window.speechSynthesis.speak(utterance)
         />
         </div>
         {/* subtitle pending */}
+        {subtitle && (
+          <div className='w-full max-w-md bg-gray-50 border border-gray-200 rounded-xl p-4 shadow-sm'>
+            <p className='text-gray-700 text-sm sm:text-base font-medium text-center leading-relaxed'>{subtitle}</p>
+          </div>
+        )}
+
+
         {/* timer area */}
         <div className='w-full max-w-md bg-white border border-gray-200 rounded-2xl shadow-md p-6 space-y-5'>
           <div className='flex justify-between items-center'>
@@ -174,12 +212,12 @@ window.speechSynthesis.speak(utterance)
           AI Smart Interview
         </h2>
 
-        <div className='relative mb-6 bg-gray-50 p-4 sm:p-6 rounded-2xl border border-gray-200 shadow-sm'>
+        {!isIntroPhase && (<div className='relative mb-6 bg-gray-50 p-4 sm:p-6 rounded-2xl border border-gray-200 shadow-sm'>
           <p className='text-xs sm:text-sm text-gray-400 mb-2'>
             Question {currentIndex+1} of {questions.length}
             </p>
           <div className='text-base sm:text-lg font-semibold text-gray-800 leading-relaxed '>{currentQuestion?.question}</div>
-        </div>
+        </div>)}
 
         <textarea 
         placeholder='Type your answer here...'
