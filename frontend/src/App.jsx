60ed6c13a63192c4579bc744afa1a64e39ad4cf3
VijaySingh092 <vijaykhetwal372@gmail.com>
2026-09-21 23:27:15 +0530

    interview history page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,9 @@ import axios from 'axios'
 import { useDispatch } from 'react-redux'
 import { setUserData } from './redux/userSlice'
 import InterviewPage from './pages/InterviewPage'
+import InterviewHistory from './pages/InterviewHistory'
+import Pricing from './pages/Pricing'
+import InterviewReport from './pages/InterviewReport'
 
 export const ServerUrl = "http://localhost:8000"
 
@@ -30,6 +33,9 @@ const dispatch = useDispatch()
       <Route path='/' element={<Home/>}/>
       <Route path='/auth' element={<Auth/>}/>
       <Route path='/interview' element={<InterviewPage/>}/>
+      <Route path='/history' element={<InterviewHistory/>}/>
+      <Route path='/pricing' element={<Pricing/>}/>
+      <Route path='/report/:id' element={<InterviewReport/>}/>
    </Routes>
   )
 }
---
+++ frontend/src/pages/InterviewReport.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const InterviewReport = () => {
+  return (
+    <div>
+      Report
+    </div>
+  )
+}
+
+export default InterviewReport
