cfbcd36ebc4127f6cc4fbde3182c22724937f978
VijaySingh092 <vijaykhetwal372@gmail.com>
2026-09-23 11:48:11 +0530

    payment router

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,7 @@ import cors from 'cors'
 import authRouter from './routes/auth.route.js'
 import userRouter from './routes/user.route.js'
 import interviewRouter from './routes/interview.route.js'
+import paymentRouter from './routes/payment.route.js'
 
 const app = express()
 const PORT= process.env.PORT || 6000
@@ -23,6 +24,8 @@ app.use("/api/auth",authRouter)
 app.use("/api/user",userRouter)
 
 app.use("/api/interview",interviewRouter)
+
+app.use("/api/payment",paymentRouter)
 
 app.get('/',(req,res)=>{
     return res.json({message:"Server Started"})
